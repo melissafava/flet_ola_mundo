ac193a2112c86134d4d4adb14f4f6839a5846e6c
Melissa <melissa.fava@aluno.senai.br>
2026-02-11 14:20:07 -0300

    colorido e centralizado

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import flet as ft
 
 def main(page: ft.Page):
+    page.title = "Meu Primeiro app flet"
+    page.bgcolor = "#f067ae"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.add(
         ft.Text(value="Olá Mundo!!!"),
         ft.ElevatedButton("Clique aqui")
